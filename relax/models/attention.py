03e6d2e7fd4fe8518c4145d893b262b78bc8bdc3
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2023-05-26 15:28:15 +0200

    Added the rotary encoding in the attention layers

--- relax/models/attention.py
+++ relax/models/attention.py
@@ -3,7 +3,7 @@
 """
 @author : Romain Graux
 @date : 2022 November 11, 17:43:12
-@last modified : 2023 May 26, 10:21:33
+@last modified : 2023 May 26, 15:15:21
 """
 
 import jax
@@ -14,9 +14,53 @@ import einops
 from relax.utils import AttrDict
 
 from typing import Optional
+from functools import cached_property
 from dataclasses import dataclass
 
 # TODO: create a faster operation for dot product attention (see deepspeed or the latest pytorch 2.0 implementation)
+
+@dataclass
+class RotaryEncoding():
+    """Rotary Encoding for attention."""
+    
+    dim : int
+    θ : float = 10_000
+
+    @cached_property
+    def inv_freq(self):
+        return 1. / (self.θ ** (jnp.arange(0, self.dim, 2, dtype=jnp.float32) / self.dim))
+
+    def pos_enc(self, t):
+        pos_enc = jnp.einsum('t, f -> tf', jnp.arange(t), self.inv_freq)
+        return einops.repeat(pos_enc, '... f -> ... (f r)', r=2)
+
+    @staticmethod
+    def rotate_half(u):
+        # last dimension of u is [u1, u2, u3, u4, ...]
+        u1, u2 = einops.rearrange(u, '... (d r) -> r ... d', r=2)
+        u = jnp.stack((-u2, u1), axis=-1)
+        # last dimension of result is [-u2, u1, -u4, u3, ...]
+        return einops.rearrange(u, '... d r -> ... (d r)')
+
+    def __call__(self, u: jnp.ndarray) -> jnp.ndarray:
+        """Rotary encoding for attention.
+
+        Args:
+            u (jnp.ndarray): either queries or keys of shape [batch_size, heads, seq_len, dim]
+
+        Returns:
+            jnp.ndarray: the rotated queries or keys of shape [batch_size, heads, seq_len, dim]
+        """
+        num_tokens = list(u.shape)[-2]
+        pos_enc = self.pos_enc(num_tokens)[:num_tokens]
+        # i % 2 == 0 : 
+        #      rotatedᵢ   = uᵢ cosθᵢ - uᵢ+1 sinθᵢ
+        #      rotatedᵢ+1 = uᵢ+1 cosθᵢ + uᵢ sinθᵢ
+        return u * jnp.cos(pos_enc) + self.rotate_half(u) * jnp.sin(pos_enc)
+
+    def rotate_qk(self, q, k):
+        # q and k are [B, H, T, D]
+        return self(q), self(k)
 
 @dataclass
 class MultiHeadAttention(hk.Module):
@@ -28,6 +72,10 @@ class MultiHeadAttention(hk.Module):
     dropout : Optional[float] = 0.0 # Dropout regularization
     rotary_encoding : Optional[bool] = False # Whether to use rotary encoding
     name : Optional[str] = None
+
+    @cached_property
+    def rotary_encoder(self):
+        return RotaryEncoding(self.embed_dim // self.num_heads)
     
     def __call__(self, q, k, v, mask=None, training=False, return_attention=True):
         assert self.embed_dim % self.num_heads == 0, "The embed dimension should be divisible by the number of heads."
@@ -40,12 +88,13 @@ class MultiHeadAttention(hk.Module):
         
         q, k, v = Q(q), K(k), V(v)
         q = rearrange(q, 'B T (h dk) -> B h T dk', h=self.num_heads) 
-        k = rearrange(k, 'B T (h dk) -> B h dk T', h=self.num_heads) # Switch the 2 last dim for the matmul with q
+        k = rearrange(k, 'B T (h dk) -> B h T dk', h=self.num_heads) 
         v = rearrange(v, 'B T (h dv) -> B h T dv', h=self.num_heads)
 
         if self.rotary_encoding:
-            raise NotImplementedError("Rotary encoding is not implemented yet.")
+            q, k = self.rotary_encoder.rotate_qk(q, k)
         
+        k = einops.rearrange(k, 'B h T d -> B h d T') # Switch the 2 last dim for the matmul with q
         key_size = self.embed_dim / self.num_heads
         attn = (q @ k) * (1 / jnp.sqrt(key_size)) 
         if mask is not None:
@@ -75,6 +124,10 @@ class SelfAttention(hk.Module):
     dropout : Optional[float] = 0.0 # Dropout regularization
     rotary_encoding : Optional[bool] = False # Whether to use rotary encoding
     name : Optional[str] = None
+
+    @cached_property
+    def rotary_encoder(self):
+        return RotaryEncoding(self.embed_dim // self.num_heads)
     
     def __call__(self, x: jnp.ndarray, mask: Optional[jnp.ndarray] = None, training: bool = False, return_attention: bool = True):
         assert self.embed_dim % self.num_heads == 0, "The embed dimension should be divisible by the number of heads."
@@ -85,12 +138,13 @@ class SelfAttention(hk.Module):
         
         q, k, v = jnp.split(qkv_w(x), 3, 2) # [B, T, C]
         q = einops.rearrange(q, 'B T (h d) -> B h T d', h=self.num_heads) 
-        k = einops.rearrange(k, 'B T (h d) -> B h d T', h=self.num_heads) # Switch the 2 last dim for the matmul with q
+        k = einops.rearrange(k, 'B T (h d) -> B h T d', h=self.num_heads)
         v = einops.rearrange(v, 'B T (h d) -> B h T d', h=self.num_heads)
 
         if self.rotary_encoding:
-            raise NotImplementedError("Rotary encoding is not implemented yet.")
+            q, k = self.rotary_encoder.rotate_qk(q, k)
         
+        k = einops.rearrange(k, 'B h T d -> B h d T') # Switch the 2 last dim for the matmul with q
         key_size = self.embed_dim / self.num_heads # d_k
         attn = (q @ k) * (1 / jnp.sqrt(key_size)) # [B, h, T, T]
         if mask is not None:
